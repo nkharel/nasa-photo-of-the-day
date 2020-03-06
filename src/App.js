2ce19beead57bdf69c45c8f84503562296026808
Nirajan Kharel <kharelnirajan@gmail.com>
2020-03-05 18:48:20 -0600

    fetched nasa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
-import "./App.css";
+// import React from "react";
+// import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>
+//         Read through the instructions in the README.md file to build your NASA
+//         app! Have fun 🚀!
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+  const [pic, setPic] = useState([]);
+  useEffect(() => {
+      axios.get("https://api.nasa.gov/planetary/apod?api_key=pVzYAArvN8c8Fez9O5lg6h3ciz0pSOuEMCPKSNav").then(response => {
+          setPic(response.data);
+      });
+  }, []);
 }
+  console.log(pic);
 
-export default App;
+  export default App;
